29d1dcbbbb16f4247a2a27c16d5e4175c908db75
AndriiKyrychenko <97938375+AndriiKyrychenko@users.noreply.github.com>
2022-02-17 08:43:28 +0200

    remove logic to this class

--- HomeWorks/src/Human.java
+++ HomeWorks/src/Human.java
@@ -14,4 +14,36 @@ public class Human {
         this.surname = surname;
         this.name = name;
     }
+
+    public static void main(String[] args) {
+
+        Human humanOne = new Human("Колокольчиков", "Николай", "Апполинариевич");
+        Human humanTwo = new Human("Виноградов", "Алексей");
+        Human[] people = {humanOne, humanTwo};
+
+        getShortName(people);
+        getFullName(people);
+    }
+
+    public static void getFullName(Human[] people) {
+        for (Human human : people) {
+            if (human.patronymic == null) {
+                System.out.println(human.surname + " " + human.name);
+            } else {
+                System.out.println(human.surname + " " + human.name + " " + human.patronymic);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void getShortName(Human[] people) {
+        for (Human human : people) {
+            if (human.patronymic == null) {
+                System.out.println(human.surname + " " + human.name.charAt(0) + ".");
+            } else {
+                System.out.println(human.surname + " " + human.name.charAt(0) + ". " + human.patronymic.charAt(0) + ".");
+            }
+        }
+        System.out.println();
+    }
 }
